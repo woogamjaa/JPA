2cc8374215518f33279073a7ea0d971253297b51
woogamjaa <woogamjaa@gmail.com>
2025-02-05 10:15:33 +0900

    JPA : @Lob , columnDefinition 사용

--- src/main/java/com/jpa/model/entity/MemberEntity.java
+++ src/main/java/com/jpa/model/entity/MemberEntity.java
@@ -61,4 +61,14 @@ public class MemberEntity {
     @Transient
     private List<String> names;
 
+    @Lob //clob
+    private String description;
+
+    @Lob //blob 정말 작은용량의 데이터 ex) 서명같은거.
+    private Byte[] bytes;
+
+    //sql문으로 컬럼을 만드는 구문을 작성
+    @Column(columnDefinition = "varchar2(20) default '없음' not null ")
+    private String phone;
+
 }
