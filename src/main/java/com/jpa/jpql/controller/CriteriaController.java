bbd547f23768125db60b50829601167cfa1d3662
woogamjaa <woogamjaa@gmail.com>
2025-02-10 13:53:59 +0900

    JPA : basicCriteria 메소드 / 다수컬럼 / select메소드를 이용해서 다수의 컬럼을 지정

--- src/main/java/com/jpa/jpql/controller/CriteriaController.java
+++ src/main/java/com/jpa/jpql/controller/CriteriaController.java
@@ -8,6 +8,8 @@ import jakarta.persistence.criteria.CriteriaBuilder;
 import jakarta.persistence.criteria.CriteriaQuery;
 import jakarta.persistence.criteria.Root;
 
+import java.util.Arrays;
+
 public class CriteriaController {
     public void basicCriteria(EntityManager em) {
         //빌더 생성하기
@@ -36,5 +38,28 @@ public class CriteriaController {
          em.createQuery(objectCriteriaQuery);
          Query query = em.createQuery(objectCriteriaQuery);
          query.getResultList().forEach(System.out::println);
+
+         //다수컬럼 선택하기
+         //Object[]로 반환함.
+         //게시글 제목, 게시글 내용
+         CriteriaQuery<Object[]> objectArrCriteriaQuery = criteriaBuilder.createQuery(Object[].class);
+        Root<BoardEntity> root3=objectArrCriteriaQuery.from(BoardEntity.class);
+        objectArrCriteriaQuery.multiselect(root3.get("boardTitle"),root3.get("boardContent"));
+        TypedQuery<Object[]> query2=em.createQuery(objectArrCriteriaQuery);
+        query2.getResultList().forEach(arr-> {
+            System.out.println(Arrays.toString(arr));
+        });
+
+        //select메소드를 이용해서 다수의 컬럼을 지정하기.
+        //CriteriaBuilder객체가 제공하는 array()메소드를 이용하면
+        //select로 다수 컬럼을 지정할 수 있다.
+        Root<BoardEntity> root4=objectArrCriteriaQuery.from(BoardEntity.class);
+        objectArrCriteriaQuery.select(
+                criteriaBuilder.array(root4.get("boardReadcount"),root4.get("boardWriter")));
+
+        query2=em.createQuery(objectArrCriteriaQuery);
+        query2.getResultList().forEach(b-> {
+            System.out.println(b[0]+" "+b[1]);
+        });
     }
 }
